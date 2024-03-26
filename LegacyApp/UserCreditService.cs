74d35dae2c38768b71214f65646da7fda7d4742c
s29076 <s29076@pjwstk.edu.pl>
2024-03-26 15:18:00 +0100

    finished

---
+++ LegacyApp/UserCreditService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace LegacyApp
+{
+    public class UserCreditService : IUserCreditService, IDisposable
+    {
+        private readonly Dictionary<string, int> _database =
+            new Dictionary<string, int>()
+            {
+                {"Kowalski", 200},
+                {"Malewski", 20000},
+                {"Smith", 10000},
+                {"Doe", 3000},
+                {"Kwiatkowski", 1000}
+            };
+
+        public void Dispose()
+        {
+        }
+
+        public int GetCreditLimit(string lastName, DateTime dateOfBirth)
+        {
+            int randomWaitingTime = new Random().Next(3000);
+            Thread.Sleep(randomWaitingTime);
+
+            if (_database.ContainsKey(lastName))
+                return _database[lastName];
+
+            throw new ArgumentException($"Client {lastName} does not exist");
+        }
+    }
+}
---
+++ LegacyApp/UserService.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace LegacyApp
+{
+    public class UserService : IUserService
+    {
+        private readonly IClientRepository _clientRepository;
+        private readonly IUserCreditService _userCreditService;
+
+        public UserService()
+        {
+            _clientRepository = new ClientRepository();
+            _userCreditService = new UserCreditService();
+        }
+
+        public bool AddUser(string firstName, string lastName, string email, DateTime dateOfBirth, int clientId)
+        {
+            if (!IsValidInput(firstName, lastName, email, dateOfBirth))
+                return false;
+
+            var client = _clientRepository.GetById(clientId);
+            if (client == null)
+                return false;
+
+            int creditLimit = DetermineCreditLimit(client, lastName, dateOfBirth);
+
+            if (creditLimit < 500 && creditLimit > 0)
+                return false;
+
+            var user = CreateUser(firstName, lastName, email, dateOfBirth, client, creditLimit);
+
+            SaveUser(user);
+
+            return true;
+        }
+
+        private bool IsValidInput(string firstName, string lastName, string email, DateTime dateOfBirth)
+        {
+            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName))
+                return false;
+
+            if (!email.Contains("@") || !email.Contains("."))
+                return false;
+
+            int age = DateTime.Now.Year - dateOfBirth.Year;
+            if (DateTime.Now.Month < dateOfBirth.Month || (DateTime.Now.Month == dateOfBirth.Month && DateTime.Now.Day < dateOfBirth.Day))
+                age--;
+
+            if (age < 21)
+                return false;
+
+            return true;
+        }
+
+        private int DetermineCreditLimit(Client client, string lastName, DateTime dateOfBirth)
+        {
+            if (client.Type == "VeryImportantClient")
+                return 0;
+
+            int creditLimit = _userCreditService.GetCreditLimit(lastName, dateOfBirth);
+
+            if (client.Type == "ImportantClient")
+                creditLimit *= 2;
+
+            return creditLimit;
+        }
+
+        private User CreateUser(string firstName, string lastName, string email, DateTime dateOfBirth, Client client, int creditLimit)
+        {
+            return new User
+            {
+                Client = client,
+                DateOfBirth = dateOfBirth,
+                EmailAddress = email,
+                FirstName = firstName,
+                LastName = lastName,
+                HasCreditLimit = creditLimit > 0,
+                CreditLimit = creditLimit
+            };
+        }
+
+        private void SaveUser(User user)
+        {
+            UserDataAccess.AddUser(user);
+        }
+    }
+}
